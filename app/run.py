687a53996ea014d9b2f1c39c212b43ed5e50f976
al-burt <berttimero@gmail.com>
2020-04-14 10:29:23 +0300

    A new URI to save userdata bi ID in memory

--- app/run.py
+++ app/run.py
@@ -1,25 +1,70 @@
 import flask
+from flask import jsonify, request
 
-from flask import jsonify
 
 app = flask.Flask(__name__)
+user_storage = {}
 
 
 @app.route("/")
 def index():
+    """
+        Initial page to return on "/" path
+
+
+        Example:
+
+        curl localhost:9000/
+
+    :return:
+    """
     return jsonify({
         "status": "ok",
         "page": "index"
     })
 
 
-@app.route("/user/get/<int:id>", methods=["GET"])
+@app.route("/user/get/<int:id>",methods=["GET"])
 def get_by_id(id):
+    """
+        URI to return data about the user for the given ID
+
+        Example:
+
+            curl localhost:9000/user/get/231
+
+
+    :param id: user_id
+    :return:
+    """
     return jsonify({
         "status": "ok",
-        "requested_id": id
+        "requested_id": id,
+        "data": user_storage.get(id,{})
     })
 
 
+@app.route("/user/add/<int:id>", methods=["POST"])
+def add_by_id(id):
+    """
+
+    Example:
+
+
+        curl -X POST localhost:9000/user/add/231   -H "Content-Type: application/json"  --data '{"name": "Ivan"}'
+
+    :param id:
+    :return:
+    """
+
+    user_data = request.json
+
+    user_storage[id] = user_data
+
+    return jsonify({
+        "status": "ok",
+        "message": "Data saved"
+    })
+
 if __name__ == '__main__':
-    app.run("0.0.0.0", 9000)
+    app.run("0.0.0.0",9000)
